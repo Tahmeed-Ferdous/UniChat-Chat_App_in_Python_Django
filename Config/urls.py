ecb5cd0f41176930283e0ad01ceb7c57c43e886c
MahediMRifat <mahedi.m.rifat@gmail.com>
2024-12-08 04:07:52 +0600

    explore error handling

--- Config/urls.py
+++ Config/urls.py
@@ -4,19 +4,20 @@ from django.conf.urls.static import static
 from django.conf import settings
 from authy.views import UserProfile, follow
 
-
 urlpatterns = [
+    # Admin URL
     path('admin/', admin.site.urls),
+    
+    # App URLs
     path('post/', include('post.urls')),
-    path('user/', include('authy.urls')),
     path('direct/', include('direct.urls')),
+    path('explore/', include('explore.urls')),  # Place the explore URLs before user-related URLs
+
+    # User Profile URLs (These are user-specific, so they should come after explore)
     path('<username>/', UserProfile, name='profile'),
     path('<username>/saved', UserProfile, name='profilefavorites'),
     path('<username>/follow/<option>', follow, name='follow'),
-    path('explore/', include('explore.urls')),
-    #path('profile/<username>/', views.UserProfile, name='profile'),
+    
+    # Other URLs can be added as necessary
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
