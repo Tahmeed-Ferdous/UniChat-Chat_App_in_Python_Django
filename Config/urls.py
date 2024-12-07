fb510d2ce00373700d64b619ba1cf6936406b037
MahediMRifat <mahedi.m.rifat@gmail.com>
2024-12-08 04:33:43 +0600

    explore redirecting issue

--- Config/urls.py
+++ Config/urls.py
@@ -4,20 +4,16 @@ from django.conf.urls.static import static
 from django.conf import settings
 from authy.views import UserProfile, follow
 
-urlpatterns = [
-    # Admin URL
-    path('admin/', admin.site.urls),
-    
-    # App URLs
-    path('post/', include('post.urls')),
-    path('direct/', include('direct.urls')),
-    path('explore/', include('explore.urls')),  # Place the explore URLs before user-related URLs
 
-    # User Profile URLs (These are user-specific, so they should come after explore)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('post/', include('post.urls')),
+    path('user/', include('authy.urls')), 
+    path('direct/', include('direct.urls')),
+    path('explore/', include('explore.urls')),
+    
+    
     path('<username>/', UserProfile, name='profile'),
     path('<username>/saved', UserProfile, name='profilefavorites'),
     path('<username>/follow/<option>', follow, name='follow'),
-
-    # Other URLs can be added as necessary
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
