b030178c27b6982820fb6a6baa7e4c8b8712d930
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2024-12-14 11:52:34 +0600

    Raw SQL Data Changeed again

--- post/views.py
+++ post/views.py
@@ -140,13 +140,13 @@ def like(request, post_id):
 
 @login_required
 def favorite(request, post_id):
-	post = Post.objects.get(id=post_id)
-	profile = Profile.objects.get(user=request.user)
+    post = Post.objects.get(id=post_id)
+    profile = Profile.objects.raw("SELECT * FROM authy_profile WHERE user_id = %s", [request.user.id]) [0]
 
-	if profile.favorites.filter(id=post_id).exists():
-		profile.favorites.remove(post)
+    if profile.favorites.filter(id=post_id).exists():
+        profile.favorites.remove(post)
 
-	else:
-		profile.favorites.add(post)
+    else:
+        profile.favorites.add(post)
 
-	return HttpResponseRedirect(reverse('postdetails', args=[post_id]))
+    return HttpResponseRedirect(reverse('postdetails', args=[post_id]))
