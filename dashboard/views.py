4e0224fdcbb59e5194689fec33454659b5eb9446
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2024-12-15 01:18:53 +0600

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,6 +2,4 @@ from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
 def dashboard(request):
-    for i in range(10):
-        print("TEST TEST TEST TEST TEST TEST TEST")
     return render(request, 'dashboard.html',locals())
