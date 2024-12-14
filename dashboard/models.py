3f904c6d5dd10ad96cfd0fc5ca37171b6b036043
Tahmeed Ferdous <tahmeed.ferdous@g.bracu.ac.bd>
2024-12-15 05:23:03 +0600

    Dashboard html and backend

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,1 +1,10 @@
 from django.db import models
+
+# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    task = models.CharField(max_length=255, default='New Task')
+
+    def __str__(self):
+        return f'{self.name}: {self.task}'
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,37 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, get_object_or_404, redirect
 from django.template import loader
 from django.http import HttpResponse
+from dashboard.models import User
+
 def dashboard(request):
-    return render(request, 'dashboard.html',locals())
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        task = request.POST.get('task')
+        if name and task:
+            User.objects.create(name=name, task=task)
+        return redirect('dashboard')
+    
+    tasks = User.objects.all()
+
+    def extract_last_two_digits(task):
+        try:
+            return int(task.task[-2:])
+        except ValueError:
+            return float('inf')
+
+    sorted_tasks = sorted(tasks, key=extract_last_two_digits)
+    return render(request, 'dashboard.html', {'tasks': sorted_tasks})
+
+def delete_task(request, task_id):
+    task = User.objects.get(id=task_id)
+    task.delete()
+    return redirect('dashboard')
+
+def edit_task(request, id):
+    task = get_object_or_404(User, id=id)
+    if request.method == 'POST':
+        task.name = request.POST.get('name')
+        task.task = request.POST.get('task')
+        task.save()
+        return redirect('dashboard')
+    return render(request, 'edit.html', {'task': task})
